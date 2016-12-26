dfe1a5796ef3d701faedc558ad811b1dbc8a4305
tkhm <devtkhm@gmail.com>
2016-12-26 15:40:04 +0900

    Fix config load setting

--- app.js
+++ app.js
@@ -31,8 +31,14 @@ app.use('/kcuc/api', (req, res) => {
   });
 });
 
-app.listen(clientPort, () => {
-  console.log('api request target is ' + target);
-  console.log('debug mode is ' + debugMode);
-  console.log('now listening on the port ' + clientPort);
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// start server on the specified port and binding host
+app.listen(appEnv.port, '0.0.0.0', function() {
+  // print a message when the server starts listening
+  console.log('server starting on ' + appEnv.url);
 });
